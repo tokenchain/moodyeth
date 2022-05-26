78d8fa9b536fe3cec1ad35fdfc961715a9f06c6e
heskdx <jobhesk@gmail.com>
2022-05-27 02:05:54 +0800

    auto patched

--- moody/flashbots/__init__.py
+++ moody/flashbots/__init__.py
@@ -3,7 +3,6 @@ from typing import Union, Optional
 from eth_account.signers.local import LocalAccount
 from eth_typing import URI
 from web3 import Web3
-from web3.middleware import geth_poa_middleware
 from web3._utils.module import attach_modules
 
 from .flashbots import Flashbots
